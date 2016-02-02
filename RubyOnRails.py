f1b93dd20a410bedba7871980ef94e90f72edf44
Klas Rogne Kanestrøm <klas.kanestrom@gmail.com>
2016-02-03 00:38:28 +0100

    Enkelt pythonScript forste commit

---
+++ RubyOnRails.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import webbrowser
+
+def start_prosjekt_rails(mappe, navnProsjekt):
+	os.chdir("%s" % mappe)
+	#os.chdir("%s" % plasering)
+	os.system("rails new %s" % navnProsjekt)
+
+def add_to_git(mappe, navnProsjekt, kommentar_git, link_github):
+	os.chdir(("%s/%s") % (mappe, navnProsjekt))
+	os.system("git init")
+	os.system("git status")
+	os.system("git add .")
+	os.system("git commit -am'%s'" % kommentar_git)
+	os.system("%s" % link_github)
+	os.system("git push -u origin master")
+	
+def rails_generate_controller(mappe, navnProsjekt, generate, action, options):
+	os.chdir(("%s/%s") % (mappe, navnProsjekt))
+	os.system("rails generate controller %s %s" % (action, options))
+
+
+
+
+
+newpro = raw_input("Vil du lage et nytt prosjektet? y/n : ")
+if newpro == "y":
+	m = raw_input("Hvor skal jeg lagre prosjektet? eks /Users/Klaskan/Documents/RailsPro ")
+	n = raw_input("Navn Prosjekt: ")
+	start_prosjekt_rails(m,n)
+
+	a = raw_input("kommentar paa commit: ")
+	webbrowser.open("https://github.com/")
+	b = raw_input("Link github: ")
+	add_to_git(m, n, a, b)
+else:
+	print "kk"
+
+
+
+gencon = raw_input("Vil du generer en controller? y/n : ")
+if gencon == "y":
+
+	rails_generate_controller()
+
+else:
+	print "kk"
